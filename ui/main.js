e99436cac557cd4f399957cd5bcf3e9dbd585d1a
chaudharyprashant <pk15196@gmail.com>
2017-02-26 01:43:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 var element=document.getElementById('maintext');
 element.innerHTML='new html';
 var img=document.getElementById('madi');
+var marginRight=0;
+function moveRight(){
+    marginRight=marginRight+10;
+    img.style.marginRight=marginRight+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
 };
