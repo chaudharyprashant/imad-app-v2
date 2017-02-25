fb495522ce8214c673a528a8f89861a37ce58c5f
chaudharyprashant <pk15196@gmail.com>
2017-02-26 01:36:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('maintext');
 element.innerHTML='new html';
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 img.onclick=function(){
     img.style.marginLeft='100px';
 };
